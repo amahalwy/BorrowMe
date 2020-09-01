2791632542bf52ea45732defd3bfd0c8049ab3ef
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-09-01 11:02:49 -0400

    Format fix on stylesheets

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,10 @@
-// src/index.js
 import './index.css'
 import React from "react";
 import ReactDOM from "react-dom";
-
-// We will create this component shortly
 import Root from "./components/root";
-
-// We set this up in the last section
 import configureStore from "./store/store";
-
-// We will use this to parse the user's session token
 import jwt_decode from "jwt-decode";
-
-// The session utility we just created
 import { setAuthToken } from "./util/session_api_util";
-
-// We have not created this action yet, but will do so in the next step
 import { logout } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
