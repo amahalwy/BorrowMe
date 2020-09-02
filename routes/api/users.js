ca9b929ce7c801e39e497473b89cb223f906d675
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-09-02 13:01:26 -0400

    Heroku Re-Build

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
-const keys = require("../../config/keys");
+const keys = require("../../config/keys_prod");
 const passport = require("passport");
 require('../../config/passport')(passport);
 const jwt = require('jsonwebtoken');
