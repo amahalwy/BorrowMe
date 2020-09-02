b40a0040c91f47670aef8ed6effacbea155e1389
ayce <raycel5lacap@gmail.com>
2020-09-02 14:51:50 -0700

    Key update
    key fix

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,8 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
+<<<<<<< HEAD
   secretOrKey: process.env.SECRET_OR_KEY
+=======
+  secretOrKey: proces.env.SECRET_OR_KEY
+>>>>>>> fcebd45... Key update
 }
