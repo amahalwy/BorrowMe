8853570f987cc01292b7743cdcc2125ac1424a46
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-09-02 13:53:27 -0400

    Ready for Heroku build

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const postings = require("./routes/api/postings");
 const bodyParser = require('body-parser');
 const passport = require('passport');
 require('./config/passport')(passport);
-// const path = require("path");
+const path = require("path");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -17,19 +17,19 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World"));
 
 app.use(passport.initialize());
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("frontend/public"));
-//   app.get("/", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/public"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
+  });
+}
 
+// app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/users", users);
 app.use("/api/postings", postings);
