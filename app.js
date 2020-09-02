e67e4fce612ae52938d37a5bcb543497746b8122
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-09-02 14:36:54 -0400

    New heroku build

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/public"));
+  app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
