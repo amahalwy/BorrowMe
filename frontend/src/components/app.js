7f00d3022272ea0185164d19f48409427fc760a4
thewizard23 <seanpscott6@gmail.com>
2020-09-01 19:30:10 -0700

    updated index css

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -16,7 +16,7 @@ const App = () => (
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginForm} />
       <AuthRoute exact path="/signup" component={SignupForm} />
-      <Route exact path="/home" component={HomePage}/>
+      <ProtectedRoute exact path="/home" component={HomePage}/>
 
       
       <ProtectedRoute exact path="/postings" component={PostingIndex}/>
