57a6000ef5139b65f63b8c0278077bf667f82c88
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-09-02 18:14:45 -0400

    Files fix

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,8 +1,4 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-<<<<<<< HEAD
   secretOrKey: process.env.SECRET_OR_KEY
-=======
-  secretOrKey: proces.env.SECRET_OR_KEY
->>>>>>> fcebd45... Key update
 }
