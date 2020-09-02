1afba5e2ac0f797967a8efeb10244b5af66c90f3
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-09-02 12:42:09 -0400

    Heroku push

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
+const db = require('./config/keys_prod').mongoURI;
 const users = require("./routes/api/users");
 const postings = require("./routes/api/postings");
 const bodyParser = require('body-parser');
-// const passport = require('passport');
-// require('./config/passport')(passport);
+const path = require("path");
+const passport = require('passport');
+require('./config/passport')(passport);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -16,12 +17,16 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use(passport.initialize());
+app.use(passport.initialize());
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
-app.get("/", (req, res) => res.send("Hello World"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/public"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
+  });
+}
 app.use("/api/users", users);
 app.use("/api/postings", postings);
-
