9dcb2dd6c8845fd2a97b6ce0f4b4f13adc20b832
ayce <raycel5lacap@gmail.com>
2020-09-04 18:15:20 -0700

    Fix

--- frontend/src/components/home/home_page.jsx
+++ frontend/src/components/home/home_page.jsx
@@ -109,31 +109,30 @@ const SearchPage = (props) => {
                   </div>
                 </div>
               </div>
-
-              <div className="engineer-info">
-                <p className="engineer-name">
-                  Nate Gallegher, Frontend Operations
-                </p>
-                <div className="links-image-container">
-                  <div className="links">
-                    <a className="links" href="https://github.com/n8gallagher">
-                      GitHub
-                    </a>
-                    <br />
-                    <a
-                      className="links"
-                      href="https://www.linkedin.com/in/n8gallagher/"
-                    >
-                      LinkedIn
-                    </a>
-                  </div>
-                  <div className="engineer-image">
-                    <img
-                      src="https://borrowme-pro.s3.us-east-2.amazonaws.com/nate.png"
-                      alt=""
-                    />
-                  </div>
-                </div>
+            </div>
+          </div>
+          <div className="engineer-quotes">
+            <p className="italicize-quote">
+              “Aside from working and leading a great group of people, I really
+              enjoyed building the auto-updating search functionality; it’s so
+              awesome!!”
+            </p>
+            - Ahmed
+          </div>
+          <div className="engineer-info">
+            <p className="engineer-name">Ayce Lacap, Backend Operations</p>
+            <div className="links-image-container">
+              <div className="links">
+                <a className="links" href="https://github.com/aycelacap">
+                  GitHub
+                </a>
+                <br />
+                <a
+                  className="links"
+                  href="https://www.linkedin.com/in/ayce-lacap-00/"
+                >
+                  LinkedIn
+                </a>
               </div>
 
               <div className="engineer-info">
@@ -159,7 +158,79 @@ const SearchPage = (props) => {
                   </div>
                 </div>
               </div>
+            </div>
+            <div className="engineer-quotes">
+              <p className="italicize-quote">
+                “I enjoyed working with implementing the CRUD features, namely,
+                update and delete. I am most proud of working collaboratively
+                with our team.”
+              </p>
+              - Ayce
+            </div>
           </div>
+          <div className="engineer-info">
+            <p className="engineer-name">Nate Gallagher, Frontend Operations</p>
+            <div className="links-image-container">
+              <div className="links">
+                <a className="links" href="https://github.com/n8gallagher">
+                  GitHub
+                </a>
+                <br />
+                <a
+                  className="links"
+                  href="https://www.linkedin.com/in/n8gallagher/"
+                >
+                  LinkedIn
+                </a>
+              </div>
+              <div className="engineer-image">
+                <img
+                  src="https://borrowme-pro.s3.us-east-2.amazonaws.com/nate.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="engineer-quotes">
+            <p className="italicize-quote">
+              "I sharpened my frontend and styling abilities on this project.
+              I'm proud of our workflow improvements over the course of the
+              project"
+            </p>
+            - Nate
+          </div>
+          <div className="engineer-info">
+            <p className="engineer-name">Sean Scott, Frontend Operations</p>
+            <div className="links-image-container">
+              <div className="links">
+                <a className="links" href="https://github.com/seanscott23">
+                  GitHub
+                </a>
+                <br />
+                <a
+                  className="links"
+                  href="https://www.linkedin.com/in/sean-scott-708821b7/"
+                >
+                  LinkedIn
+                </a>
+              </div>
+              <div className="engineer-image">
+                <img
+                  src="https://borrowme-pro.s3.us-east-2.amazonaws.com/sean.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="engineer-quotes">
+            <p className="italicize-quote">
+              "I’m most proud of my work on the frontend. More specifically, my
+              work on the index page as well as the different modals we used in
+              the project."
+            </p>
+            - Sean
+          </div>
+        </div>
       </div>
       <div className="home-index">
         <div>
