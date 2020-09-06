f1f0f2e9bb8ca9497555f4eb86dbddad21ca665c
thewizard23 <seanpscott6@gmail.com>
2020-09-05 19:43:51 -0700

    Updated rent button

--- frontend/src/components/calendar/calendar.jsx
+++ frontend/src/components/calendar/calendar.jsx
@@ -1,19 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import { DateRange } from "react-date-range";
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 import { Discovery } from "aws-sdk";
+import FormData from "form-data";
+import {useSelector} from 'react-redux';
 
-export default props => {
+
+export default (props) => {
   const [state, setState] = useState([{
       startDate: new Date(),
       endDate: null,
       key: "selection",
     }, ]);
 
+  const currentUser = useSelector(state => state.session.user); 
     // useEffect(() => {
-      
-    // }, [state.endDate])
+    //   setStart(props.calendarEvent.state);
+    //   setEnd(props.calendarEvent.state);
+    // }, [state.StatDate, state.endDate]);
+
 
     const getDates = (state) => {
       let dateArray = new Array();
@@ -24,9 +30,25 @@ export default props => {
       }
       return dateArray;
     }
+   
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("postingId", props.posting._id);
+    formData.append("requestorId", currentUser.id );
+    formData.append("startDate", state[0].startDate);
+    formData.append("endDate", state[0].endDate);
+    debugger
+
+      return axios
+        .post("/api/requests", formData)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+  };
     
-    
-    return ( 
+    return (
+      <div>
         <DateRange
           editableDateInputs={true}
           onChange={(item) => setState([item.selection])}
@@ -34,6 +56,9 @@ export default props => {
           ranges={state}
         />
 
-
+        <button className="modal-rent-button" onClick={handleSubmit}>
+          Rent now!
+        </button>
+      </div>
     );
 }
