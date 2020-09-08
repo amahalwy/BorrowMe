cfaf64f6b4b50355c1d6fca2f44c8526466e01ac
ayce <raycel5lacap@gmail.com>
2020-09-08 07:55:39 -0700

    Place second marker on map for current user

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
+import { useSelector } from 'react-redux';
 import mapboxgl from "mapbox-gl";
 const token = require('../../config/keys').mapBoxToken;
 
@@ -9,9 +10,10 @@ const Map = (props) => {
   
   const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
-  
-  const { address, city, state, zipCode } = props.posting
+  const currentUser = useSelector((state) => state.session.user);
 
+  const searchTextCurrentUser = `${currentUser.address} ${currentUser.city} ${currentUser.state} ${currentUser.zipCode}`;
+  const { address, city, state, zipCode } = props.posting;
   const searchText = `${address} ${city} ${state} ${zipCode}`;
 
   useEffect(() => {
@@ -41,64 +43,85 @@ const Map = (props) => {
       setMap(map);
       
 
-      // Change this up to hold the currentUser stuff
-      // Grab currentUser address, state, etc and convert it
-      let marker = new mapboxgl.Marker()
-        .setLngLat([-122.414, 37.776])
-        .addTo(map);
-
-      // let searchText = "123 kissling street san francisco CA 94103"
       let request = new Request(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?country=US&access_token=${token}`);
       const res = await fetch(request);
       const json = await res.json();
       const coordinates = json.features[0].geometry.coordinates;
-      // console.log(coordinates);
-      // .then(function (response.json()) {
-      //   console.log(response.json());
-      //   return response.json();
-      // }).then( json => {
-      //   const data = json.features[0].geometry.coordinates;
-      //   console.log(data);
-      // })
       
       let markerPlace = new mapboxgl.Marker()
         .setLngLat([coordinates[0], coordinates[1]])
         .addTo(map);
 
+      // find currentUser's information
+      let requestCurrentUser = new Request(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchTextCurrentUser}.json?country=US&access_token=${token}`);
+      const resCurrentUser = await fetch(requestCurrentUser);
+      const jsonCurrentUser = await resCurrentUser.json();
+      const coordinatesCurrentUser = jsonCurrentUser.features[0].geometry.coordinates;
+
+      let markerPlaceCurrentUser = new mapboxgl.Marker()
+        .setLngLat([coordinatesCurrentUser[0], coordinatesCurrentUser[1]])
+        .addTo(map);
+
+      function getRoute(coordinates) {
+        // make a directions request using cycling profile
+        // an arbitrary start will always be the same
+        // only the end or destination will change
+        let start = [coordinatesCurrentUser[0], coordinatesCurrentUser[1]];
+        let url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + coordinates[0] + ',' + coordinates[1] + '?steps=true&geometries=geojson&access_token=' + `${token}`;
+
+        // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+        let req = new XMLHttpRequest();
+        req.open('GET', url, true);
+        req.onload = function () {
+          let jsonRoute = JSON.parse(req.response);
+          let data = jsonRoute.routes[0];
+          let route = data.geometry.coordinates;
+          let geojson = {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: route
+            }
+          };
+          // if the route already exists on the map, reset it using setData
+          if (map.getSource('route')) {
+            map.getSource('route').setData(geojson);
+          } else { // otherwise, make a new request
+            map.addLayer({
+              id: 'route',
+              type: 'line',
+              source: {
+                type: 'geojson',
+                data: {
+                  type: 'Feature',
+                  properties: {},
+                  geometry: {
+                    type: 'LineString',
+                    coordinates: geojson
+                  }
+                }
+              },
+              layout: {
+                'line-join': 'round',
+                'line-cap': 'round'
+              },
+              paint: {
+                'line-color': '#3887be',
+                'line-width': 5,
+                'line-opacity': 0.75
+              }
+            });
+          }
+          // add turn instructions here at the end
+        };
+        req.send();
+      }
+
 
     };
     if (!map) initializeMap({ setMap, mapContainer });
   }, [map]);
-
-
-  // // 3rd try
-  let geojson = {
-    type: "FeatureCollection",
-    features: [
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [-77.032, 38.913],
-        },
-        properties: {
-          title: "Mapbox",
-          description: "Washington, D.C.",
-        },
-      },
-      {
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [-122.414, 37.776],
-        },
-        properties: {
-          title: "Mapbox",
-          description: "San Francisco, California",
-        },
-      },
-    ],
-  };
 
   return (
     <div>
