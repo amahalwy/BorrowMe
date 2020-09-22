bfecf50683056bb472d51b11cdec397e25ea79c2
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-09-22 19:53:42 -0400

    Bug fix when loading profile

--- frontend/src/components/requests/posting_requestor_show.jsx
+++ frontend/src/components/requests/posting_requestor_show.jsx
@@ -1,40 +1,43 @@
 import React from "react";
 
 export default (props) => {
-  if (!props.requestDates || props.requestDates === undefined) return '';
-  // const currentUser = useSelector((state) => state.session.user);
-  const totalAmount = props.amount * props.requestDates.length;
 
-  return (
-    <div className="modal-main-show">
-      <div>
-        <button className="modal-x" onClick={props.hideModal}>
-          X
-        </button>
+  if (!props.requestDates || props.requestDates === undefined) {
+    return ''
+  } else {
+    
+  const totalAmount = props.amount * props.requestDates.length;
+    return (
+      <div className="modal-main-show">
+        <div>
+          <button className="modal-x" onClick={props.hideModal}>
+            X
+          </button>
+        </div>
+        <div>
+          <span className="request-title">{props.title}</span>
+        </div>
+        <div>
+          <span className="total-amount">
+            Total cost for {props.requestDates.length} days: ${totalAmount}
+          </span>
+        </div>
+        <div>
+          <span>Requestor: {props.user}</span>
+        </div>
+        <div>
+          <img className="request-image" src={props.image} />
+        </div>
+        <div></div>
+        <div className="request-buttons">
+          <span>
+            <button className="accept-request">Accept</button>
+          </span>
+          <span>
+            <button className="reject-request">Reject</button>
+          </span>
+        </div>
       </div>
-      <div>
-        <span className="request-title">{props.title}</span>
-      </div>
-      <div>
-        <span className="total-amount">
-          Total cost for {props.requestDates.length} days: ${totalAmount}
-        </span>
-      </div>
-      <div>
-        <span>Requestor: {props.user}</span>
-      </div>
-      <div>
-        <img className="request-image" src={props.image} />
-      </div>
-      <div></div>
-      <div className="request-buttons">
-        <span>
-          <button className="accept-request">Accept</button>
-        </span>
-        <span>
-          <button className="reject-request">Reject</button>
-        </span>
-      </div>
-    </div>
-  );
+    );
+  }
 };
