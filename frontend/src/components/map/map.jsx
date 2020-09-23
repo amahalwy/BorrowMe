c5e5aac9d8baa3714b331270a1661d8527697830
Ahmed El Mahallawy <amahalwy@gmail.com>
2020-09-22 21:35:37 -0400

    Map fix

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -4,48 +4,43 @@ import mapboxgl from "mapbox-gl";
 const token = require('../../config/keys').mapBoxToken;
 
 const Map = (props) => {
-  const lng = -122.44;
-  const lat = 37.76;
-  const zoom = 11;
-
-  const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
+  const [map, setMap] = useState("");
   const currentUser = useSelector((state) => state.session.user);
-
   const searchTextCurrentUser = `${currentUser.address} ${currentUser.city} ${currentUser.state} ${currentUser.zipCode}`;
   const { address, city, state, zipCode } = props.posting;
-  const searchText = `${address} ${city} ${state} ${zipCode}`;
+  const postingAddress = `${address} ${city} ${state} ${zipCode}`;
 
   useEffect(() => {
-    mapboxgl.accessToken = token;
-    const bounds = [
-      [-122.54, 37.6], // [west, south]
-      [-122.34, 37.9], // [east, north]
-    ];
+    handleMap();
+  }, [map]);
 
-    const initializeMap = async ({ setMap }) => {
+  const handleMap = () => {
+
+    mapboxgl.accessToken = token;
+
+    const initializeMap = async ({ setMap, mapContainer }) => {
+
       const map = new mapboxgl.Map({
-        container: mapContainer.current, // container id
-        style: "mapbox://styles/mapbox/streets-v11",
-        center: [lng, lat], // starting position
-        zoom: zoom, // starting zoom
+        // the type of map we want to have
+        // the latLong info
+        // ref mapContainer, current allows us to access everything in the dom for that specific element
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/light-v10",
+        center: ["-122.44", "37.76"],
+        zoom: 11,
       });
 
-      map.addControl(new mapboxgl.NavigationControl());
-      map.addControl(
-        new mapboxgl.GeolocateControl({
-          positionOptions: {
-            enableHighAccuracy: true,
-          },
-          trackUserLocation: true,
-        })
-      );
-      map.setMaxBounds(bounds);
       setMap(map);
 
+
+      /**
+       This is the First Marker
+       */
       let request = new Request(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchText}.json?country=US&access_token=${token}`
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${postingAddress}.json?country=US&access_token=${token}`
       );
+
       const res = await fetch(request);
       const json = await res.json();
       const coordinates = json.features[0].geometry.coordinates;
@@ -54,6 +49,9 @@ const Map = (props) => {
         .setLngLat([coordinates[0], coordinates[1]])
         .addTo(map);
 
+      /**
+       * This is the Second Marker
+       */
       // find currentUser's information
       let requestCurrentUser = new Request(
         `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchTextCurrentUser}.json?country=US&access_token=${token}`
@@ -65,26 +63,27 @@ const Map = (props) => {
 
       let markerPlaceCurrentUser = new mapboxgl.Marker()
         .setLngLat([coordinatesCurrentUser[0], coordinatesCurrentUser[1]])
-        .addTo(map);
+        .addTo(map)
 
-      function getRoute(coordinates) {
-        // make a directions request using cycling profile
-        // an arbitrary start will always be the same
-        // only the end or destination will change
-        let start = [coordinatesCurrentUser[0], coordinatesCurrentUser[1]];
+      /**
+       * asychronous call to receiver the routes data between 2 points
+       */
+
+      const getRoute = () => {
+        const start = [coordinatesCurrentUser[0], coordinatesCurrentUser[1]];
         let url =
           "https://api.mapbox.com/directions/v5/mapbox/cycling/" +
-          start[0] +
+          `${start[0]}` +
           "," +
-          start[1] +
+          `${start[1]}` +
           ";" +
-          coordinates[0] +
+          `${coordinates[0]}` +
           "," +
-          coordinates[1] +
+          `${coordinates[1]}` +
           "?steps=true&geometries=geojson&access_token=" +
           `${token}`;
 
-        // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+
         let req = new XMLHttpRequest();
         req.open("GET", url, true);
         req.onload = function () {
@@ -99,11 +98,9 @@ const Map = (props) => {
               coordinates: route,
             },
           };
-          // if the route already exists on the map, reset it using setData
           if (map.getSource("route")) {
             map.getSource("route").setData(geojson);
           } else {
-            // otherwise, make a new request
             map.addLayer({
               id: "route",
               type: "line",
@@ -129,119 +126,48 @@ const Map = (props) => {
               },
             });
           }
-          // add turn instructions here at the end
         };
         req.send();
-      }
+      };
+      getRoute();
+
+      map.on('load', () => {
+        const defaultCoords = {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: [],
+          },
+        };
+
+        map.addSource("routes", {
+          type: "geojson",
+          data: defaultCoords,
+        });
+        getRoute();
+      })
     };
+    // if no map, update dom
     if (!map) initializeMap({ setMap, mapContainer });
-  }, [map]); // eslint-disable-line react-hooks/exhaustive-deps
+  };
+
 
   return (
     <div>
-      <button onClick={props.hideModal}>XXXXX</button>
-      <div>
-        <div>Your address: {currentUser.address}</div>
-        <div>Item address: {props.posting.address}</div>
+      <button className="close-map-x" onClick={props.hideModal}>X</button>
+      <div className="maps-info-header">
+        <img className="maps-item-img-sm" src={props.posting.image} alt="image of item" />
+        <div>
+          <div><span>{props.posting.title}</span></div>
+          <div>Your address: {currentUser.address}</div>
+          <div>Item address: {postingAddress}</div>
+        </div>
       </div>
-      <div>
-        <button>Find fastest route!</button>
-      </div>
-      <div
-        ref={(el) => (mapContainer.current = el)}
-        className="map_container"
-      >
-      </div>
+      <div ref={(el) => (mapContainer.current = el)} className="container-for-map"></div>
     </div>
   );
-};
-
+}
 
 export default Map;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from "react";
-// import MapGL, { GeolocateControl } from "react-map-gl";
-// import   from "";
-// import "mapbox-gl/dist/mapbox-gl.css";
-
-// const TOKEN = accessToken;
-
-// const geolocateStyle = {
-//   float: "left",
-//   margin: "50px",
-//   padding: "10px",
-// };
-
-// const Map = () => {
-//   const [viewport, setViewPort] = useState({
-//     width: "100%",
-//     height: 900,
-//     latitude: 0,
-//     longitude: 0,
-//     zoom: 2,
-//   });
-
-//   const _onViewportChange = (viewport) =>
-//     setViewPort({ ...viewport, transitionDuration: 3000 });
-
-//   return (
-//     <div style={{ margin: "0 auto" }}>
-//       <h1
-//         style={{ textAlign: "center", fontSize: "25px", fontWeight: "bolder" }}
-//       >
-//         GeoLocator: Click To Find Your Location or click{" "}
-//         <a href="/search">here</a> to search for a location
-//       </h1>
-//       <MapGL
-//         {...viewport}
-//         mapboxApiAccessToken={TOKEN}
-//         mapStyle="mapbox://styles/mapbox/dark-v8"
-//         onViewportChange={_onViewportChange}
-//       >
-//         <GeolocateControl
-//           style={geolocateStyle}
-//           positionOptions={{ enableHighAccuracy: true }}
-//           trackUserLocation={true}
-//         />
-//       </MapGL>
-//     </div>
-//   );
-// };
-
-// export default Map;
-
-
-
-
-
-
 
